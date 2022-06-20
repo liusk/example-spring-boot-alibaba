32dc402c85360082a8b8d27c3a8ad79b39efe227
liusk <lsk@servyou.com.cn>
2022-06-20 15:33:50 +0800

    11111

---
+++ stream-rocketmq/src/main/java/com/example/streamrocketmq/StreamRocketmqApplication.java
@@ -0,0 +1,17 @@
+package com.example.streamrocketmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.cloud.stream.messaging.Source;
+
+@SpringBootApplication
+@EnableBinding({Source.class, Sink.class})
+public class StreamRocketmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamRocketmqApplication.class, args);
+	}
+
+}
---
+++ stream-rocketmq/src/test/java/com/example/streamrocketmq/StreamRocketmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.streamrocketmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamRocketmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
